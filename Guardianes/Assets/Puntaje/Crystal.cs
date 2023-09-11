078059f8988049b5f87c84df131ffe99c14eef08
ClaudiaFlorez <cmflorezg14@gmail.com>
2023-09-11 01:13:55 -0500

    Adding Assets

---
+++ Guardianes/Assets/Puntaje/Crystal.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Crystal : MonoBehaviour
+{
+    [SerializeField] private AudioSource audioSource;
+    [SerializeField] private AudioClip sonido;
+    [SerializeField] private GameObject particulas;
+
+    public void CogerCristal()
+    {
+        if (audioSource != null && sonido != null)
+            audioSource.PlayOneShot(sonido);
+
+        Instantiate(particulas, transform.position, transform.rotation);
+
+       
+    }
+    public void AutoDestroy()
+    {
+              Destroy(gameObject);
+    }
+}
